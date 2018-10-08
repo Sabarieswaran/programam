a610f9457a74a5dd07164c54ccfd8b689ec33992
Sabarieswaran <41147402+Sabarieswaran@users.noreply.github.com>
2018-10-08 14:46:36 +0530

    Add files via upload

---
+++ sub.py
@@ -0,0 +1,16 @@
+import ipaddress
+import math
+addr=input("Enter the ip:")
+#host=ipaddress.ip_address(addr)
+ip=addr.split("/")
+p=ip[1]
+d=32-int(p)
+try:
+	m=math.pow(2,d)
+	n=ipaddress.IPv4Network(addr)
+	first,last=n[0],n[-1]
+	print("ADDRESS BLOCK:",int(m))
+	print("FIRST ADDRESS:",first)
+	print("LAST ADDRESS:",last)
+except:
+	print("INVALID HOST BITS")
